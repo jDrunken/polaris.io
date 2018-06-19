964d9c2423a9dd6222d0b5bcf4f5ba4f5d12ca9e
Jinhun Shin <jinhun@chainpartners.net>
2018-06-19 23:24:53 +0900

    the 12th commit

--- source/js/js.js
+++ source/js/js.js
@@ -162,6 +162,8 @@
     var _isSetClassName = true;
 
     function smoothScrollTo(hash, e) {
+        $('body').removeClass('posting');
+        $('#policy,#term,#disclimer').removeClass('viewing');
         if (hash === '') { return false; }
 
         if ($(hash).length > 0) {
@@ -214,7 +216,6 @@
             headerInteraction($('#header'),$(window),null);
         });
 
-
         // header의 interaction 처리
         $('#header nav a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(e) {
             $('#header a[href*="#"]').removeClass('viewing');
@@ -241,3 +242,161 @@
         });
     });
 })(jQuery);
+
+
+// --------------------------------------------------------------------------------
+// form에 focus되면 label의 position을 옆으로 옮김
+// --------------------------------------------------------------------------------
+
+(function ($) {
+    $(function (){
+        var target = $('#contact label+input, #contact textarea')
+        ,   textarea = $('#contact textarea')
+
+        $(target).focus(function (){
+            $(this).prev('label').addClass('focused')
+        });
+
+        $(target).blur(function (){
+            var $this = $(this);
+            if(!$this.val()) {
+                $(this).prev('label').removeClass('focused')
+            }
+        });
+
+        // textarea.keydown(function () {
+        //     $(this).css({
+        //         'height':'auto',
+        //     });
+        //     $(this).height(this.scrollHeight );
+        // });
+
+
+    });
+})(jQuery);
+
+
+
+// --------------------------------------------------------------------------------
+// 서버리스 메일 처리
+// --------------------------------------------------------------------------------
+function validEmail(email) { // see:
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
+
+function validateHuman(honeypot) {
+    if (honeypot) {  //if hidden form filled up
+        console.log("Robot Detected!");
+        return true;
+    } else {
+        console.log("Welcome Human!");
+    }
+}
+
+// get all data in form and return object
+function getFormData() {
+    var form = document.getElementById("gform");
+    var elements = form.elements; // all form elements
+    var fields = Object.keys(elements).filter(function(k) {
+        // the filtering logic is simple, only keep fields that are not the honeypot
+        return (elements[k].name !== "honeypot");
+    }).map(function(k) {
+        if(elements[k].name !== undefined) {
+            return elements[k].name;
+            // special case for Edge's html collection
+        }else if(elements[k].length > 0){
+            return elements[k].item(0).name;
+        }
+    }).filter(function(item, pos, self) {
+        return self.indexOf(item) == pos && item;
+    });
+    var data = {};
+    fields.forEach(function(k){
+        data[k] = elements[k].value;
+        var str = ""; // declare empty string outside of loop to allow
+        // it to be appended to for each item in the loop
+        if(elements[k].type === "checkbox"){ // special case for Edge's html collection
+            str = str + elements[k].checked + ", "; // take the string and append
+            // the current checked value to
+            // the end of it, along with
+            // a comma and a space
+            data[k] = str.slice(0, -2); // remove the last comma and space
+            // from the  string to make the output
+            // prettier in the spreadsheet
+        }else if(elements[k].length){
+            for(var i = 0; i < elements[k].length; i++){
+                if(elements[k].item(i).checked){
+                    str = str + elements[k].item(i).value + ", "; // same as above
+                    data[k] = str.slice(0, -2);
+                }
+            }
+        }
+    });
+
+    // add form-specific values into the data
+    data.formDataNameOrder = JSON.stringify(fields);
+    data.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+    data.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+
+    console.log(data);
+    return data;
+}
+
+function handleFormSubmit(event) {  // handles form submit without any jquery
+    event.preventDefault();           // we are submitting via xhr below
+    var data = getFormData();         // get the values submitted in the form
+
+    // /* OPTION: Remove this comment to enable SPAM prevention, see README.md
+    if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
+        return false;
+    }
+    // */
+
+    if( data.email && !validEmail(data.email) ) {   // if email is not valid show error
+        var invalidEmail = document.getElementById("email-invalid");
+        if (invalidEmail) {
+            invalidEmail.style.display = "block";
+            return false;
+        }
+    } else {
+        var url = event.target.action;  //
+        var xhr = new XMLHttpRequest();
+
+        disableAllButtons(event.target);
+
+        // jquery로 변경해야되나..
+        xhr.open('POST', url);
+        // xhr.withCredentials = true;
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            console.log( xhr.status, xhr.statusText )
+            console.log(xhr.responseText);
+            var thankYouMessage = document.getElementById("thankyou_message");
+            if (thankYouMessage) {
+                thankYouMessage.style.display = "block";
+            }
+            return;
+        };
+        // url encode form data for sending as post data
+        var encoded = Object.keys(data).map(function(k) {
+            return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
+        }).join('&')
+        xhr.send(encoded);
+    }
+}
+function loaded() {
+    console.log("Contact form submission handler loaded successfully.");
+    // bind to the submit event of our form
+    var form = document.getElementById("gform");
+    form.addEventListener("submit", handleFormSubmit, false);
+};
+document.addEventListener("DOMContentLoaded", loaded, false);
+
+function disableAllButtons(form) {
+    var buttons = form.querySelectorAll("button");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+    }
+}
+//
